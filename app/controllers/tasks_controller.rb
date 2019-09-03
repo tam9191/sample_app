a8b4856ce446feabb7a78b5914fa4dc96928aad5
EC2 Default User <ec2-user@ip-172-31-47-70.us-east-2.compute.internal>
2019-09-03 10:50:50 +0000

    C5,C6途中

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,30 @@
 class TasksController < ApplicationController
+  before_action :set_user
+  
   def index
-    @user = User.find(params[:id])
-    @task = Task.find(params[:user_id])
+    @task = @user.tasks
   end
+  
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+     @task = @user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to user_tasks_url @user
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
